4d08400bbf2f39c3e70123b626fd800db01e701f
LEEYUHC <leah.crowe@noaa.gov>
2019-03-19 15:44:08 -0400

    two digit day, month2

--- scripts/NARWSSserver.R
+++ scripts/NARWSSserver.R
@@ -1281,7 +1281,8 @@ observeEvent(input$rawupload,{
       ############
       
       month1<-month.abb[month(as.character(final$DateTime[1]))]
-      day1<-day(final$DateTime[1])
+      month2<-format.Date(final$DateTime[1], "%m")
+      day1<-format.Date(final$DateTime[1], "%d")
       year1<-year(final$DateTime[1])
       date1<-paste0(day1,' ',month1,' ',year1)
 
