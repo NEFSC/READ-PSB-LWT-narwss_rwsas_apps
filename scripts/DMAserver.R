39c2100e688f9a17ca90f9e556a582aa569dc093
LEEYUHC <leah.crowe@noaa.gov>
2020-02-10 17:07:22 -0500

    upload

---
+++ scripts/DMAserver.R
@@ -0,0 +1,120 @@
+
+disable("dmaup")
+disable("dmareport")
+disable("kml")
+
+loc<-"Network"
+    ###########
+    
+observeEvent(input$query,{
+  
+      dmaevaldate<-input$sasdate    
+      print(dmaevaldate)
+      
+      fish <- odbcConnect(server, uid=sasuid, pwd=saspswd,believeNRows=FALSE)
+      
+      datesql<-paste0("select rightwhalesight.sas.ID, SIGHTDATE,GROUPSIZE,LAT,LON,SPECIES_CERT,MOMCALF,FEEDING,DEAD,SAG,ENTANGLED,CATEGORY,rightwhalesight.action.action,OBSERVER_PEOPLE,OBSERVER_PLATFORM,OBSERVER_ORG,REPORTER_PEOPLE,REPORTER_PLATFORM,REPORTER_ORG,WHALEALERT,OBSERVER_COMMENTS
+                from rightwhalesight.sas,rightwhalesight.action
+                where trunc(sightdate) = to_date('",dmaevaldate,"','YYYY-MM-DD')
+                      and LAT > 36.5
+                      and SPECIES_CERT = 3
+                      and rightwhalesight.sas.action = rightwhalesight.action.ID
+                order by ID;") 
+      
+      dailyeg<-sqlQuery(fish,datesql)
+      
+      if(nrow(dailyeg) == 0){
+        ##if no Eg:
+        output$error1<-renderText({"No right whales were reported for this day"})
+        disable("eval")
+      }
+      else {
+      output$error1<-renderText({""})  
+      enable("eval")
+      dailyeg$SIGHTDATE<-ymd_hms(dailyeg$SIGHTDATE, tz = "America/New_York")
+      dailyeg$LAT<-sprintf("%.5f",round(dailyeg$LAT, digits = 5))
+      dailyeg$LON<-sprintf("%.5f",round(dailyeg$LON, digits = 5))
+      
+      dailyeg[] <- lapply(dailyeg, as.character)
+      
+      dailyeg<-dailyeg%>%
+        mutate(Select = TRUE)%>%
+        dplyr::select(Select, everything())
+      
+      dailyeghot<-rhandsontable(dailyeg,readOnly = TRUE)%>%
+        hot_table(highlightCol = TRUE, highlightRow = TRUE)%>%
+        hot_cols(columnSorting = TRUE)%>%
+        hot_col("SIGHTDATE", width = 150)%>%
+        hot_col("Select", readOnly = FALSE)
+        
+      output$dailyeghot = renderRHandsontable({dailyeghot})
+}
+})
+      
+observeEvent(input$eval,{
+
+      dmaevaldate<-input$sasdate
+      print(dmaevaldate)
+      egtable = hot_to_r(input$dailyeghot)
+      print(egtable)
+      egtable<-egtable%>%
+        filter(Select == TRUE)%>%
+        dplyr::select(-Select)
+      
+      print(egtable)
+      
+      egtable$LAT<-as.numeric(egtable$LAT)
+      egtable$LON<-as.numeric(egtable$LON)
+      ###############
+      ##SMA evaluation
+      ########
+      smapath<-"./SMA ind shp"
+      MODA<-unique(format(dmaevaldate, "%m-%d"))
+      MODAYR<-unique(dmaevaldate, "%m-%d")
+
+      ##egtable & egsas kept seperate like this for now, need to investigate more about how these tables are used between dma and narwss apps 11/20/2018 lmc
+      egtable<-egtable%>%
+        dplyr::rename("DateTime" = "SIGHTDATE","LATITUDE" = "LAT", "LONGITUDE" = "LON", "GROUP_SIZE" = "GROUPSIZE", "ID_RELIABILITY" = "SPECIES_CERT")
+      
+      egsas<-egtable %>% 
+        dplyr::select(ID,DateTime,GROUP_SIZE,LATITUDE,LONGITUDE,ID_RELIABILITY,MOMCALF,FEEDING,DEAD,SAG,ENTANGLED,CATEGORY,ACTION,OBSERVER_COMMENTS,OBSERVER_PEOPLE,OBSERVER_PLATFORM,OBSERVER_ORG)
+
+      egsas$DateTime<-strftime(egsas$DateTime,'%Y-%m-%d %H:%M:%S')
+      
+      ##############
+      ## FAKE DMA ##
+      ##############
+      fakedma<-data.frame(
+        long = c(-71,-71,-71,-71,-71),
+        lat = c(42,42,42,42,42))
+      
+      fakedma<-Polygons(list(Polygon(fakedma, hole=as.logical(NA))), ID = 1)
+      ##############
+      
+      source('./scripts/oracleaccess.R', local = TRUE)$value
+      source('./scripts/sma.R', local = TRUE)$value
+      source('./scripts/activedma.R', local = TRUE)$value
+      
+      #####
+      ##egtable for SAS
+        output$error1<-renderText({""})
+        
+        month1<-month.abb[month(as.character(egsas$DateTime[1]))]
+        month2<-format.Date(egsas$DateTime[1], "%m")
+        day1<-format.Date(egsas$DateTime[1], "%d")
+        year1<-year(egsas$DateTime[1])
+        date1<-paste0(day1,' ',month1,' ',year1)
+     
+        #this is for the user input for the observer in the dma letter
+        triggrptrue<-TRUE
+        
+      source('./scripts/action & dma.R', local = TRUE)$value
+      
+      
+      #############
+      
+      
+})  #24
+    
+
+  
