168497918f8bc400a01799e11eb3d586cee4dd46
leahcrowe <leah.crowe@noaa.gov>
2021-04-02 13:47:25 -0500

    ship icon

--- app.R
+++ app.R
@@ -26,7 +26,7 @@ ui <- dashboardPage(
       menuItem(icon = icon("plane"),"Aerial Survey Processing App", tabName = "NARWSS"),
       menuItem(icon = icon("calendar"),"Trigger Analysis", tabName = "DMA"),
       menuItem(icon = icon("camera"),"Photo Position Finder", tabName = "Photo"),
-      menuItem(icon = icon(""),"SLOW zone viewer", tabName = "szone"),
+      menuItem(icon = icon("ship"),"SLOW zone viewer", tabName = "szone"),
       menuItem(icon = icon("question-circle"),text = "Instructions Wiki", href = "https://github.com/leahcrowe/narwss_rwsas_apps/wiki")
     )
   ),
