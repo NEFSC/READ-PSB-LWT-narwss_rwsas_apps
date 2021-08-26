abe0eb8ba7812607314fd20b1ee58e19de12c9a4
heather-foley-noaa <heather.foley@noaa.gov>
2021-08-26 17:34:28 -0400

    add in test choice

--- scripts/DMAui.R
+++ scripts/DMAui.R
@@ -1,7 +1,7 @@
 fluidPage(
   useShinyjs(),
   titlePanel("Trigger analysis for DMAs and other protection areas from visual and acoustic detections"),
-  splitLayout(radioButtons("sig_acou", "What do you want to evaluate?", choices = c("Visual Sightings", "Acoustic Detections"), selected = "Visual Sightings", inline = FALSE),
+  splitLayout(radioButtons("sig_acou", "What do you want to evaluate?", choices = c("Visual Sightings", "Acoustic Detections", "Test"), selected = "Visual Sightings", inline = FALSE),
               dateInput("sasdate", "Date:")),
                            actionButton("query","Query Database"),
                            textOutput("error1"),
