f64ef60c8d751b930bb03c2c8d40740d699bb059
leahcrowe <leah.crowe@noaa.gov>
2022-10-13 23:12:36 +1300

    reformat, dma -> slowzone

--- scripts/download_content.R
+++ scripts/download_content.R
@@ -1,33 +1,35 @@
+# conditional report info ----
 
-  print(tempdir())
+print(tempdir())
 
-if (ftype == 20){
-  ftypesent<-"Only large whale sightings were recorded on this survey."
-} else if (ftype == 21){
-  ftypesent<-"Only large whale sightings (excluding live minke whales) were recorded on this survey."
+if (ftype == 20) {
+  ftypesent <-
+    "Only large whale sightings were recorded on this survey."
+} else if (ftype == 21) {
+  ftypesent <-
+    "Only large whale sightings (excluding live minke whales) were recorded on this survey."
 } else {
-  ftypesent<-""
+  ftypesent <- ""
 }
 
-rptnotes<-input$reportnotes
+rptnotes <- input$reportnotes
 
-if (file.exists('./scripts/oracleaccess.R') == TRUE){
-  
-  dmanamesexpsent<-paste0("Active right whale SLOW zone(s): ",dmanamesexp,".")
-  webshotpath<-paste0(getwd(),"/surveymap.png")
+if (file.exists('./scripts/oracleaccess.R') == TRUE) {
+  dmanamesexpsent <-
+    paste0("Active right whale SLOW zone(s): ", dmanamesexp, ".")
+  webshotpath <- paste0(getwd(), "/surveymap.png")
   source('./scripts/oracleaccess.R', local = TRUE)$value
   source('./scripts/input_sas.R', local = TRUE)$value
-  source('./scripts/input_dma.R', local = TRUE)$value
+  source('./scripts/input_slowzone.R', local = TRUE)$value
   
 } else {
-  
   disable("dmaup")
   disable("dmareport")
   disable("kml")
   disable("dmaletter")
   
-  dmanamesexpsent<-""
-  webshotpath<-paste0(path,"surveymap.png")
+  dmanamesexpsent <- ""
+  webshotpath <- paste0(path, "surveymap.png")
   
 }
 
@@ -36,10 +38,22 @@ print("webshot")
 
 file.copy("FlightReport.Rmd", tempReport, overwrite = FALSE)
 
-params<-list(date1 = date_formats$date1, rptnotes = rptnotes, reportmap = reportmap, netable = netable, egreport = egreport, dmanamesexpsent = dmanamesexpsent, ftypesent = ftypesent, webshotpath = webshotpath)
+params <-
+  list(
+    date1 = date_formats$date1,
+    rptnotes = rptnotes,
+    reportmap = reportmap,
+    netable = netable,
+    egreport = egreport,
+    dmanamesexpsent = dmanamesexpsent,
+    ftypesent = ftypesent,
+    webshotpath = webshotpath
+  )
 print(webshotpath)
 print(params)
-rmarkdown::render(tempReport, output_file = file,
-                  params = params,
-                  envir = new.env(parent = globalenv())
+rmarkdown::render(
+  tempReport,
+  output_file = file,
+  params = params,
+  envir = new.env(parent = globalenv())
 )
