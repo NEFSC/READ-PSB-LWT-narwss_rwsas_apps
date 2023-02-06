17ca3a181d7e5c063640dcebc8c23700f7e5b4ac
leahcrowe <leah.crowe@noaa.gov>
2023-02-07 07:48:05 +1300

    formatting

--- scripts/photo_ui.R
+++ scripts/photo_ui.R
@@ -1,26 +1,55 @@
-fluidPage(  
+fluidPage(
   useShinyjs(),
   titlePanel("Where are the whales?"),
   sidebarLayout(
     sidebarPanel(
-      radioButtons("filepathway", "File Pathway", choices = c("Network", "Local"), selected = "Network", inline = FALSE), 
-      textInput("filepathinput", (HTML(paste("Local pathway for edit_data folders and photo submission csv", '<br/>', "Example: C:/Users/leah.crowe/Desktop/Canada Data Processing/")))),
-      radioButtons("tzone", "Camera Time Zone", choices = c("Atlantic Time","Eastern Time"), selected = "Eastern Time", inline = FALSE),
-      textInput("photoyear","Year (YYYY)"),
-      textInput("photofile", "Filename (must be a csv, but do not enter the extension)"),
+      radioButtons(
+        "filepathway",
+        "File Pathway",
+        choices = c("Network", "Local"),
+        selected = "Network",
+        inline = FALSE
+      ),
+      textInput("filepathinput", (HTML(
+        paste(
+          "Local pathway for edit_data folders and photo submission csv",
+          '<br/>',
+          "Example: C:/Users/leah.crowe/Desktop/Canada Data Processing/"
+        )
+      ))),
+      radioButtons(
+        "tzone",
+        "Camera Time Zone",
+        choices = c("Atlantic Time", "Eastern Time"),
+        selected = "Eastern Time",
+        inline = FALSE
+      ),
+      textInput("photoyear", "Year (YYYY)"),
+      textInput(
+        "photofile",
+        "Filename (must be a csv, but do not enter the extension)"
+      ),
       textInput("permit", "Permit Number", placeholder = "MMPA #####"),
       actionButton("photogo", "Get positions")
-      ),
+    ),
     mainPanel(
-      (HTML(paste('<br/>',
-                  "CSV must include these columns:",'<br/>',
-                  '<br/>',
-                  "Field EGNO, EG Letter, Local Time, Day, Month, Year, Latitude, Longitude, Area, Obs, Platform, Image Type, Assoc. Type, Behaviors, Notes, Photographer, Frames, First Edit, Second Edit, Final Edit",
-                  '<br/>','<br/>',
-                  'Latitude, Longitude, Area, Obs, Platform, and Image Type <strong>can be blank</strong>.',
-                  '<br/>','<br/>',
-                  "Local Time and EG Letter should <strong>not be blank</strong>.",
-                  '<br/>','<br/>'))),
+      (HTML(
+        paste(
+          '<br/>',
+          "CSV must include these columns:",
+          '<br/>',
+          '<br/>',
+          "Field EGNO, EG Letter, Local Time, Day, Month, Year, Latitude, Longitude, Area, Obs, Platform, Image Type, Assoc. Type, Behaviors, Notes, Photographer, Frames, First Edit, Second Edit, Final Edit",
+          '<br/>',
+          '<br/>',
+          'Latitude, Longitude, Area, Obs, Platform, and Image Type <strong>can be blank</strong>.',
+          '<br/>',
+          '<br/>',
+          "Local Time and EG Letter should <strong>not be blank</strong>.",
+          '<br/>',
+          '<br/>'
+        )
+      )),
       br(),
       textOutput("finalmess"),
       br(),
