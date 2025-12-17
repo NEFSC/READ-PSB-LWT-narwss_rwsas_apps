fd56581c1b3cf3b8abde34258a2f3930a78b548b
Heather Foley <heather.foley@noaa.gov>
2025-12-17 12:54:05 -0500

    goodbye phantomjs hello mapview::mapshot2 and chromium

--- scripts/download_content.R
+++ scripts/download_content.R
@@ -16,8 +16,8 @@ rptnotes <- input$reportnotes
 
 if (file.exists('./scripts/oracleaccess.R') == TRUE) {
   dmanamesexpsent <- paste0("Active right whale SLOW zone(s): ", dmanamesexp, ".")
-  #webshotpath <- paste0(getwd(), "/surveymap.png") #works with Phantomjs 1/3
-  webshotpath <- file.path(getwd(), "surveymap.png") #don't think this will work - likely  need to delete
+  webshotpath <- paste0(getwd(), "/surveymap.png") #works with Phantomjs 1/3
+  #webshotpath <- file.path(getwd(), "surveymap.png") #alternative to above
   
   source('./scripts/oracleaccess.R', local = TRUE)$value
   source('./scripts/input_sas.R', local = TRUE)$value
@@ -30,28 +30,21 @@ if (file.exists('./scripts/oracleaccess.R') == TRUE) {
   disable("dmaletter")
   
   dmanamesexpsent <- ""
-  #webshotpath <- paste0(path, "surveymap.png") #works with Phantomjs 2/3
-  webshotpath <- file.path(path, "surveymap.png") #ChatGPT's attempt with mapview/webshot
+  webshotpath <- paste0(path, "surveymap.png") #works with Phantomjs 2/3
+  #webshotpath <- file.path(path, "surveymap.png") #alternative to above -
 }
 print("line 36")
 #Save leaflet map to PNG w/o PhantomJS or Chromium
 #saved as "temp.html" html widget in NARWSServer 1984 and/or input_slowzone 16 selfcontained = FALSE
 
-#mapview::mapshot2(reportmap, file = webshotpath, selfcontained = TRUE) #needs widget first, not "temp.html" #mapview option
-webshot::webshot("temp.html", file = webshotpath) #works with webshot::phantomjs 3/3
+mapview::mapshot2(reportmap, file = webshotpath, selfcontained = TRUE) #needs widget first, not "temp.html" #mapview option
+#webshot::webshot("temp.html", file = webshotpath) #works with webshot::phantomjs 3/3
 #webshot2::webshot( #Option 3 - webshot2
 #"temp.html",
 #file = webshotpath,
 #)
 
-#print(paste("Map saved to", webshotpath))
-#print(class(reportmap))
-#print(class(date_formats$date1)) #needs to be a string or a date for mapview to work
-#print("webshot")
-
 file.copy("FlightReport.Rmd", tempReport, overwrite = FALSE)
-
-#print(date_formats)
 
 params <- list(
     date1 = date_formats$date1,
@@ -64,9 +57,6 @@ params <- list(
     webshotpath = webshotpath
   )
 print(webshotpath)
-#print(params) #trying to troubleshoot $ operator is invalid for atomic vectors errors which assume comes from date1 = date_formats$date1 above
-#print(file)
-#print(str(params))
 
 rmarkdown::render(
   tempReport,
@@ -74,4 +64,3 @@ rmarkdown::render(
   params = params,
   envir = new.env(parent = globalenv())
 )
-
