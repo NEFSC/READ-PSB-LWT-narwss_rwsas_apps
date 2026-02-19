56db0478854862a50d6cb10648814ba018df6dc9
Heather Foley <heather.foley@noaa.gov>
2026-02-19 15:59:40 -0500

    add back in for on-demand/in-app WM data pull. For loop removed on WM data pull (git ignore file) with WriteTable for cloud-based Oracle DB writes. -HJF

--- scripts/trigger_server.R
+++ scripts/trigger_server.R
@@ -38,11 +38,11 @@ if (file.exists('./scripts/oracleaccess.R') == TRUE) {
     print(dmaevaldate)
     #for testing  dmaevaldate <- '2024-03-13'
     
-    
     ## VISUAL SIGHTINGS ----
     if (input$sig_acou == 'Visual Sightings') {
-      #source('./scripts/Whalemap_datapull.R', local = TRUE)$value THIS IS SEVERLY SLOWING DOWN APP AND CAUSING ISSUES
-      #stick with cronjob and add back in if we can figure it out in a R Env
+      source('./scripts/Whalemap_datapull.R', local = TRUE)$value 
+      #20260219 add back in with new pathways and no for loops - keep on demand WM pull if not too slow
+      # plan to keep cronjob(?) and add back in if we can figure it out in a R Env
       
       datesql <-
         paste0(
