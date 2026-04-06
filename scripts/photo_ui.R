4e0d4bba6949da0c32aa965ce9c010909670644c
Heather Foley <heather.foley@noaa.gov>
2026-04-06 11:39:56 -0400

    clean up

--- scripts/photo_ui.R
+++ scripts/photo_ui.R
@@ -13,13 +13,13 @@ fluidPage(
       ),
       textInput("filepathinput", (HTML(
         paste(
-          "Insert Local pathway for edit_data folders and photo submission csv",
+          "Insert Local pathway for edit_data folders and photo submission csv *only if not working from server*",
           '<br/>',
           "Example: C:/Users/allison.henry/Desktop/Flights/",
           '<br/>',
           "NETWORK NOTE: .csv needs to be in Image Submission Folder directly, not a subfolder",
           '<br/>',
-          "Network path: nefscdata/PSD-Whale_Surveys/Fieldwrk/Aerials/Year/Year_Digital_Photos/Image Submission/"
+          "Network path: Nefscdata/PSD-Whale_Surveys/Fieldwrk/Aerials/Year/Year_Digital_Photos/Image Submission/"
         )
       ))),
       radioButtons(
