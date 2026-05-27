4bc3067bbd063a037bc652a2c1a7cc69799b18d1
bmckenna2 <brigid.mckenna@noaa.gov>
2026-05-27 13:36:34 -0400

    updates for local shiny flight report using webshot in lieu of mapshot2

--- scripts/download_content.R
+++ scripts/download_content.R
@@ -23,39 +23,42 @@ if (file.exists('./scripts/oracleaccess.R') == TRUE) {
   source('./scripts/input_sas.R', local = TRUE)$value
   source('./scripts/input_slowzone.R', local = TRUE)$value
   
+  print("line 36")
+  #Save leaflet map to PNG w/o PhantomJS or Chromium
+  #saved as "temp.html" html widget in NARWSServer 1984 and/or input_slowzone 16 selfcontained = FALSE
+  
+  mapview::mapshot2(reportmap, file = webshotpath, selfcontained = TRUE) #
+  
 } else {
   disable("dmaup")
   disable("dmareport")
   disable("kml")
   disable("dmaletter")
+  require(webshot)
   
   dmanamesexpsent <- ""
   webshotpath <- paste0(path, "surveymap.png") #works with Phantomjs 2/3
   #webshotpath <- file.path(path, "surveymap.png") #alternative to above -
+  print("line 36")
+  webshot::webshot("temp.html", file = webshotpath) #works with webshot::phantomjs 3/3
 }
-print("line 36")
+
 #Save leaflet map to PNG w/o PhantomJS or Chromium
 #saved as "temp.html" html widget in NARWSServer 1984 and/or input_slowzone 16 selfcontained = FALSE
 
-mapview::mapshot2(reportmap, file = webshotpath, selfcontained = TRUE) #needs widget first, not "temp.html" #mapview option
-#webshot::webshot("temp.html", file = webshotpath) #works with webshot::phantomjs 3/3
-#webshot2::webshot( #Option 3 - webshot2
-#"temp.html",
-#file = webshotpath,
-#)
 
 file.copy("FlightReport.Rmd", tempReport, overwrite = FALSE)
 
 params <- list(
-    date1 = date_formats$date1,
-    rptnotes = rptnotes,
-    reportmap = reportmap,
-    netable = netable,
-    egreport = egreport,
-    dmanamesexpsent = dmanamesexpsent,
-    ftypesent = ftypesent,
-    webshotpath = webshotpath
-  )
+  date1 = date_formats$date1,
+  rptnotes = rptnotes,
+  reportmap = reportmap,
+  netable = netable,
+  egreport = egreport,
+  dmanamesexpsent = dmanamesexpsent,
+  ftypesent = ftypesent,
+  webshotpath = webshotpath
+)
 print(webshotpath)
 
 rmarkdown::render(
